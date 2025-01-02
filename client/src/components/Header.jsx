7be001b1147a8a350f2d40916505dd78728a2c5c
Bash-Abdul <bashirtaiwo59@gmail.com>
2025-01-02 13:40:22 +0000

    media query and client side done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import Logo from '../assets/thumbnail.jpg'
 import { FaBars } from "react-icons/fa";
@@ -6,22 +6,33 @@ import {AiOutlineClose} from 'react-icons/ai'
 import "../index.css"
 
 const Header = () => {
+  const [navToggle, setNavToggle] = useState(window.innerWidth > 800 ? true : false);
+
+  const closeNav = ()=>{
+    window.innerWidth < 800 ? setNavToggle(false) : setNavToggle(true)
+  }
   return (
     <nav>
       <div className="container nav__container">
-        <Link to={"/"} >
+        <Link to={"/"} onClick={closeNav}>
           <img src={Logo} alt='Navbar Logo' className='nav_logo' />
           </Link>
 
-          <ul className='nav__menu'>
-            <li><Link to={'/profile/:id'}>Bash Abdul</Link></li>
-            <li><Link to={'/create'}>Create Post</Link></li>
-            <li><Link to={'/authors'}>Authors</Link></li>
-            <li><Link to={'/logout'}>Logout</Link></li>
+          {
+            navToggle && (
+              <ul className='nav__menu'>
+            <li><Link to={'/profile/:id'} onClick={closeNav}>Bash Abdul</Link></li>
+            <li><Link to={'/create'} onClick={closeNav}>Create Post</Link></li>
+            <li><Link to={'/authors'} onClick={closeNav}>Authors</Link></li>
+            <li><Link to={'/logout'} onClick={closeNav}>Logout</Link></li>
           </ul>
+            )
+          }
 
-          <button className='nav__toggle-btn'>
-            <AiOutlineClose/>
+          <button className='nav__toggle-btn' onClick={()=> setNavToggle(!navToggle)}>
+            {
+              navToggle ? <AiOutlineClose/> : <FaBars/>
+            }
           </button>
       </div>
     </nav>
